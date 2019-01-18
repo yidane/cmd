ea79e9b36d392f2686580a00279a6d5f2ed18787
yidane <yidane@163.com>
2019-01-18 18:08:41 +0800

    update func defaultParse,fix bugs for resolve nothing from args

--- internal/defaultParse.go
+++ internal/defaultParse.go
@@ -1,9 +1,12 @@
 package internal
 
 import (
+	"bytes"
+	"container/list"
 	"fmt"
 	"github.com/yidane/cmd/opt"
 	"strings"
+	"unicode"
 )
 
 type DefaultParse struct {
@@ -15,48 +18,80 @@ func (DefaultParse) ParseArg(arg string) (args []*opt.Arg, err error) {
 		return
 	}
 
-	argArr := strings.Split(arg, " ")
+	l := readStringAsList(arg)
+	k, v := "", ""
+	node := l.Front()
+	for node != nil {
+		arg := node.Value.(string)
 
-	for i := 1; i < len(argArr); i++ {
-		arg := strings.TrimSpace(argArr[i])
-		switch len(arg) {
-		case 0:
+		if arg[0] != '-' {
+			v = arg
+			args = append(args, opt.NewArg(len(args), k, v))
+			k, v = "", ""
+			node = node.Next()
 			continue
-		case 1:
-			err = fmt.Errorf("bad flag syntax: %s", arg)
-			return
-		default:
-			if arg[0] != '-' {
+		}
+
+		if arg[1] == '-' {
+			if len(arg) == 1 {
 				err = fmt.Errorf("bad flag syntax: %s", arg)
 				return
 			}
-			if arg[1] == '-' {
-				arg = arg[2:]
-			}
+			arg = arg[2:] //set --arg as -arg
 		}
 
-		k, v := "", ""
 		if strings.Contains(arg, "=") { //if container "=",arg and value is here,otherwise the value is next arg
-			kv := strings.SplitN(arg, "=", 2)
+			kv := strings.Split(arg, "=")
 			k = kv[0]
-			v = kv[1]
-		} else {
-			k = arg
-			if i == len(args)-1 { //if it is the last one,it could be bool and set is true
-				v = "true"
-			} else {
-				arg1 := strings.TrimSpace(argArr[i+1])
-				if arg1[0] == '-' {
-					v = "true"
-				} else {
-					v = arg1
-					i++
-				}
-			}
+			v = strings.Join(kv[1:], "=")
+
+			args = append(args, opt.NewArg(len(args), k, v))
+			k, v = "", ""
+			node = node.Next()
+			continue
 		}
 
-		newArg := opt.NewArg(0, k, v)
-		args = append(args, newArg)
+		k = arg
+		node = node.Next()
+		if node == nil {
+			args = append(args, opt.NewArg(len(args), k, v))
+			continue //end for
+		}
+
+		for node != nil {
+			arg = node.Value.(string)
+			if arg[0] == '-' { //another k
+				args = append(args, opt.NewArg(len(args), k, v))
+				k, v = "", ""
+				break
+			}
+
+			//one key match many value
+			v = arg
+			args = append(args, opt.NewArg(len(args), k, v))
+			node = node.Next()
+		}
+	}
+
+	return
+}
+
+func readStringAsList(arg string) (l list.List) {
+	l = list.List{}
+	buf := bytes.Buffer{}
+	for _, v := range strings.TrimSpace(arg) {
+		if unicode.IsSpace(v) {
+			if buf.Len() > 0 {
+				l.PushBack(buf.String())
+				buf.Reset()
+			}
+		} else {
+			buf.WriteRune(v)
+		}
+	}
+	if buf.Len() > 0 {
+		l.PushBack(buf.String())
+		buf.Reset()
 	}
 
 	return
--- internal/defaultParse_test.go
+++ internal/defaultParse_test.go
@@ -1,17 +1,41 @@
 package internal
 
 import (
+	"strconv"
 	"testing"
 )
 
 func TestDefaultParse_ParseArg(t *testing.T) {
-	args, err := new(DefaultParse).ParseArg("./mdImage -rf /tmp/Database/")
+	args, err := new(DefaultParse).ParseArg("./mdImage -rf    	/tmp/Database/ /sa/ -i -s=y -y")
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	for _, v := range args {
-		t.Log(v.Key(), "=", v.Value())
+		t.Log("[", v.Key(), "=", v.Value(), "]")
 	}
-	//t.Log("nothing implement")
+}
+
+func Test_readStringAsList(t *testing.T) {
+	arg := `1 2  3   4   5    6	7		8			9
+							10   
+11
+12 13	14`
+
+	l := readStringAsList(arg)
+
+	node := l.Front()
+	i := 1
+	for node != nil {
+		j, _ := strconv.Atoi(node.Value.(string))
+		if i != j {
+			t.Fatal(i, j)
+		}
+		node = node.Next()
+		i++
+	}
+
+	if i != 15 {
+		t.Fatal(i)
+	}
 }
